24d63153cf959fc52c61b469e4722d0cbd91a552
T0a5t-E <gerardogongora.personal@gmail.com>
2025-10-07 05:26:07 -0600

    feat(sidebar): add support for section headers in navigation links

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,6 +14,7 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://kit.fontawesome.com/a9fa881c04.js" crossorigin="anonymous"></script>
+
     <!-- Styles -->
     @livewireStyles
 </head>
